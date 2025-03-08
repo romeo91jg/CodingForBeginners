c688b446ac1b6cbe3335461b7d0b0185167c530a
romeo91jg <rgaralde91@yahoo.com>
2025-03-08 15:49:33 -0800

    Add files via upload

---
+++ assigning values.py
@@ -0,0 +1,16 @@
+a = 8
+b = 4
+print('Assign Values:\t' , 'a =' , a , '\tb=' , b)
+
+a += b
+print('Add & Assign:\t' , 'a =' , a , '\t(8 +=4)' )
+
+a -= b
+print('Subtract & Assign:\t' , a , '\t(12 - 4)' )
+a *= b
+print('Multiply * Assign:\t' , a , '\t(8 x 4)' )
+
+a /= b
+print('Divide & Assign:\t' , a , '\t(32 / 4)' )
+a %= b
+print('Remainder & Assign:\t' , a , '\t(8 % 4)' )
